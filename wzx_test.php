52d74b9253ef7ad82db161e300d948ccf01f5a85
unknown <knight@knight-PC.(none)>
2012-05-17 09:07:06 +0800

    Signed-off-by: knight <knight@knight-PC.(none)>

--- wzx_test.php
+++ wzx_test.php
@@ -4,9 +4,9 @@ $_CONFIG['DBNAME']='classpub';
 require_once('AQFrameWork/AQFrameWork.php');
 
 
-$m=new Model('message');
-$args['uid']=2;
-$r=$m->fetchMessage($args);
+$m=new Model('relation');
+$args['uid']=3;
+$r=$m->getStudents($args);
 echo $m->getLastSql();
 var_dump($r);
 ?>
