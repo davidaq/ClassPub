66dd43b65ce3e2f367775a5d6330eff08e6ec2ac
davidaq <153019747@qq.com>
2012-05-18 13:15:12 +0800

    go on with relations

--- Model/relation.mod.php
+++ Model/relation.mod.php
@@ -3,24 +3,34 @@
 
 %getClassMates		//获取用户的所有同学
 $uid
-SELECT cid
-FROM `{$TP}S_C` X
-WHERE `X.uid`!=$uid AND `X.cid`IN(SELECT cid
-									FROM `{$TP}S_C` Y
-									WHERE `Y.uid`=$uid)
+SELECT `name`,`uid` FROM `{$TP}user`
+WHERE `uid` IN(
+	SELECT `X`.`uid`
+		FROM `{$TP}s_c` X
+		WHERE `X`.`uid`!=$uid AND `X`.`cid` IN (SELECT cid
+			FROM `{$TP}s_c` Y
+			WHERE `Y`.`uid`=$uid)
+)
 
 %getTeachers			//获取用户所有的老师
 $uid
-SELECT teacher 
-FROM `{$TP}class`
-WHERE `cid` IN (SELECT cid
-             FROM `{$TP}S_C`
-			 WHERE `uid`=$uid)
+SELECT `name`,`uid` FROM `{$TP}user`
+WHERE `uid` IN(
+	SELECT teacher
+		FROM `{$TP}class`
+		WHERE `cid` IN (SELECT cid
+			FROM `{$TP}s_c`
+			WHERE `uid`=$uid)
+)
 
 %getStudents			//获取用户所有的学生
 $uid
-SELECT uid
-FROM `{$TP}S_C`
-WHERE `cid` IN (SELECT cid
-			  FROM `{$TP}class`
-			  WHERE `teacher`=$uid)
+
+SELECT `name`,`uid` FROM `{$TP}user`
+WHERE `uid` IN(
+	SELECT uid
+		FROM `{$TP}s_c`
+		WHERE `cid` IN (SELECT cid
+			FROM `{$TP}class`
+			WHERE `teacher`=$uid)
+)
